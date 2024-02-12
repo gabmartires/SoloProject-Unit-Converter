22267f15862a0485c19478c8c15cbd336ce7fa94
Gabriel Martires <gmartires23@gmail.com>
2024-02-12 16:57:59 -0600

    JS Input Value Calculations

--- index.js
+++ index.js
@@ -6,39 +6,52 @@ const liters = 3.7854
 const kilo = 2.204623
 const pound = 0.4535924
 let convertBtn = document.getElementById("convert-btn")
-let unitEl = document.getElementById("unit-el")
+let inputEl = document.getElementById("input-el")
 let lengthEl = document.getElementById("lenght-el")
 let volumeEl = document.getElementById("volume-el")
 let massEl = document.getElementById("mass-el")
+let toNumber = JSON.parse(localStorage.getItem("unit"))
+
+console.log(toNumber[0])
+console.log(typeof toNumber)
 
 convertBtn.addEventListener("click", function(){
-    unit.push(unitEl.value) 
-    unitEl.value = '';   
-    console.log(unit)
-    toDOM()
+    unit.push(inputEl.value)
+    inputEl.value = '';      
+    localStorage.setItem("keyVal", JSON.stringify(unit))    
+    toDOM()    
 })
 
-function convert() {
+
+
+function convert() {     
      let feetToMeters = ""
      let meterToFeet = ""
      let litersToGallons = ""
      let galToLiters = ""
      let kiloToPounds = ""
      let poundToKilos = ""
-     feetToMeters = unit * meter
-     meterToFeet = unit * feet
-     litersToGallons = unit * gallon
-     galToLiters = unit * liters
-     kiloToPounds = unit * kilo
-     poundToKilos = unit * pound
+     feetToMeters = toNumber[0] * meter
+     meterToFeet = toNumber[0]* feet
+     litersToGallons = toNumber[0]* gallon
+     galToLiters = toNumber[0]* liters
+     kiloToPounds = toNumber[0] * kilo
+     poundToKilos = toNumber[0] * pound
      return [feetToMeters, meterToFeet, litersToGallons, galToLiters, kiloToPounds, poundToKilos]
 }
 
 let result = convert()
 
 function toDOM() {
-    lengthEl.innerHTML = `<strong>${unit} meters</strong> = ${result[1].toFixed(3)} feet  |  <strong>${unit} feet</strong> = ${result[0].toFixed(3)} meters`
-    volumeEl.innerHTML = `<strong>${unit} liters</strong> = ${result[2].toFixed(3)} gallons  |  <strong>${unit} gallons</strong> = ${result[3].toFixed(3)} liters`
-    massEl.innerHTML = `<strong>${unit} kilos</strong> = ${result[4].toFixed(3)} pounds  |  <strong>${unit} punds</strong> = ${result[5].toFixed(3)} kilos`  
+    lengthEl.innerHTML = `<strong>${unit[0]} meters</strong> = ${result[1].toFixed(3)} feet  |  <strong>${unit[0]} feet</strong> = ${result[0].toFixed(3)} meters`
+    volumeEl.innerHTML = `<strong>${unit[0]} liters</strong> = ${result[2].toFixed(3)} gallons  |  <strong>${unit[0]} gallons</strong> = ${result[3].toFixed(3)} liters`
+    massEl.innerHTML = `<strong>${unit[0]} kilos</strong> = ${result[4].toFixed(3)} pounds  |  <strong>${unit[0]} punds</strong> = ${result[5].toFixed(3)} kilos`  
 }
 
+function clear() {
+    if(unit.length === 2) {
+        toNumber = localStorage.removeItem("keyVal")
+    }
+}
+
+clear()
